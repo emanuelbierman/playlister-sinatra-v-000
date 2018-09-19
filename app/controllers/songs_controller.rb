ebe8f40d3f2f059a55f35a2782a62484e9060042
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-19 21:43:34 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -40,7 +40,7 @@ class SongsController < ApplicationController
     erb :'/songs/edit'
   end
 
-  patch '/songs/:slug' do
+  post '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
