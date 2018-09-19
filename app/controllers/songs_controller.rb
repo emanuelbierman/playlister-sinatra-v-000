fbef79af00d8dd2a8ca52f5e310041c9cffa06aa
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-19 20:33:47 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,20 +26,12 @@ class SongsController < ApplicationController
     if !params[:genres].blank?
       @song.genre_ids = params[:genres]
     end
-    # if params[:new_artist_name].blank?
-    #   @artist = Artist.find_by(params[:artist_name])
-    #   @song.artist = @artist
-    # else
-    #   @artist = Artist.find_or_create_by(name: params[:new_artist_name])
-    #   @song.artist = @artist
-    # end
+    
     @artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.update(artist: @artist)
-    # @song.save
+
     flash[:message] = "Successfully created song."
     redirect("/songs/#{@song.slug}")
-    # redirect "/songs/#{@song.slug}"
-    # redirect to("/songs/#{@song.slug}")
   end
 
   get '/songs/:slug/edit' do
